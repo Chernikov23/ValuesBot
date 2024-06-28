0b0d7d8d32df851cbf520392e116050240ccbdfd
Alexander <118567820+Chernikov23@users.noreply.github.com>
2024-06-28 14:29:44 +0300

    usd and eur

--- handlers/bot_messages.py
+++ handlers/bot_messages.py
@@ -40,6 +40,38 @@ async def fetch_crypto_data():
             return data
 
 
+async def fetch_usd_rub():
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            "https://ru.investing.com/currencies/usd-rub"
+        ) as response:
+            html = await response.text()
+            soup = BeautifulSoup(html, "html.parser")
+            price_tag = soup.find(
+                "div",
+                class_="text-5xl/9 font-bold text-[#232526] md:text-[42px] md:leading-[60px]",
+            )
+            if price_tag:
+                return price_tag.text.strip()
+            return None
+
+
+async def fetch_eur_rub():
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            "https://ru.investing.com/currencies/eur-rub"
+        ) as response:
+            html = await response.text()
+            soup = BeautifulSoup(html, "html.parser")
+            price_tag = soup.find(
+                "div",
+                class_="text-5xl/9 font-bold text-[#232526] md:text-[42px] md:leading-[60px]",
+            )
+            if price_tag:
+                return price_tag.text.strip()
+            return None
+
+
 @router.inline_query()
 async def inline_query_handler(query: InlineQuery):
     query_text = query.query.upper()
@@ -48,88 +80,143 @@ async def inline_query_handler(query: InlineQuery):
         results = [
             InlineQueryResultArticle(
                 id="help",
-                title="Введите символ криптовалюты",
+                title="Введите символ криптовалюты или валютной пары",
                 input_message_content=InputTextMessageContent(
-                    message_text="Введите символ криптовалюты, например, BTC или ETH."
+                    message_text="Введите символ криптовалюты, например, BTC или валютной пары, например, USD или EUR."
                 ),
             )
         ]
     else:
-        data = await fetch_crypto_data()
         results = []
-        for name, price in data:
-            if query_text in name:
+        if query_text == "USD":
+            price = await fetch_usd_rub()
+            if price:
                 results.append(
                     InlineQueryResultArticle(
-                        id=name,
-                        title=f"{name}",
-                        description=f"Текущий курс: {price}",
+                        id="USD_RUB",
+                        title="USD/RUB",
+                        description=f"Текущий курс: {price} RUB",
                         input_message_content=InputTextMessageContent(
-                            message_text=f"Текущий курс {name}: {price}",
+                            message_text=f"Текущий курс USD/RUB: {price} RUB"
                         ),
                         reply_markup=InlineKeyboardMarkup(
                             inline_keyboard=[
                                 [
                                     InlineKeyboardButton(
-                                        text="Получить курс",
-                                        callback_data=f"get_price:{name}",
+                                        text="Обновить", callback_data="update:USD_RUB"
                                     )
                                 ]
                             ]
                         ),
                     )
                 )
+        elif query_text == "EUR":
+            price = await fetch_eur_rub()
+            if price:
+                results.append(
+                    InlineQueryResultArticle(
+                        id="EUR_RUB",
+                        title="EUR/RUB",
+                        description=f"Текущий курс: {price} RUB",
+                        input_message_content=InputTextMessageContent(
+                            message_text=f"Текущий курс EUR/RUB: {price} RUB"
+                        ),
+                        reply_markup=InlineKeyboardMarkup(
+                            inline_keyboard=[
+                                [
+                                    InlineKeyboardButton(
+                                        text="Обновить", callback_data="update:EUR_RUB"
+                                    )
+                                ]
+                            ]
+                        ),
+                    )
+                )
+        else:
+            data = await fetch_crypto_data()
+            for name, price in data:
+                if query_text in name:
+                    results.append(
+                        InlineQueryResultArticle(
+                            id=name,
+                            title=f"{name}",
+                            description=f"Текущий курс: {price}",
+                            input_message_content=InputTextMessageContent(
+                                message_text=f"Текущий курс {name}: {price}",
+                            ),
+                            reply_markup=InlineKeyboardMarkup(
+                                inline_keyboard=[
+                                    [
+                                        InlineKeyboardButton(
+                                            text="Получить курс",
+                                            callback_data=f"get_price:{name}",
+                                        )
+                                    ]
+                                ]
+                            ),
+                        )
+                    )
 
     await query.answer(results, cache_time=1)
 
 
-@router.callback_query(lambda c: c.data.startswith("get_price:"))
+@router.callback_query(
+    lambda c: c.data.startswith("get_price:") or c.data.startswith("update:")
+)
 async def process_callback(callback_query: CallbackQuery):
-    crypto_name = callback_query.data.split(":")[1]
-    data = await fetch_crypto_data()
-    price = None
-    for name, crypto_price in data:
-        if name == crypto_name:
-            price = crypto_price
-            break
-    if price:
-        await bot.edit_message_text(
-            text=f"Текущий курс {crypto_name}: {price}",
-            inline_message_id=callback_query.inline_message_id,
-            reply_markup=InlineKeyboardMarkup(
-                inline_keyboard=[
-                    [
-                        InlineKeyboardButton(
-                            text="Обновить", callback_data=f"update:{crypto_name}"
-                        )
+    query_type, symbol = callback_query.data.split(":")
+    if query_type == "get_price":
+        data = await fetch_crypto_data()
+        price = None
+        for name, crypto_price in data:
+            if name == symbol:
+                price = crypto_price
+                break
+        if price:
+            await bot.edit_message_text(
+                text=f"Текущий курс {symbol}: {price}",
+                inline_message_id=callback_query.inline_message_id,
+                reply_markup=InlineKeyboardMarkup(
+                    inline_keyboard=[
+                        [
+                            InlineKeyboardButton(
+                                text="Обновить", callback_data=f"update:{symbol}"
+                            )
+                        ]
                     ]
-                ]
-            ),
-        )
-    await bot.answer_callback_query(callback_query.id)
-
-
-@router.callback_query(lambda c: c.data.startswith("update:"))
-async def process_update(callback_query: CallbackQuery):
-    crypto_name = callback_query.data.split(":")[1]
-    data = await fetch_crypto_data()
-    price = None
-    for name, crypto_price in data:
-        if name == crypto_name:
-            price = crypto_price
-            break
-    if price:
-        await bot.edit_message_text(
-            text=f"Текущий курс {crypto_name}: {price}",
-            inline_message_id=callback_query.inline_message_id,
-            reply_markup=InlineKeyboardMarkup(
-                inline_keyboard=[
-                    [
-                        InlineKeyboardButton(
-                            text="Обновить", callback_data=f"update:{crypto_name}"
-                        )
-                    ]
-                ]
-            ),
-        )
+                ),
+            )
+    elif query_type == "update":
+        if symbol == "USD_RUB":
+            price = await fetch_usd_rub()
+            if price:
+                await bot.edit_message_text(
+                    text=f"Текущий курс USD/RUB: {price} RUB",
+                    inline_message_id=callback_query.inline_message_id,
+                    reply_markup=InlineKeyboardMarkup(
+                        inline_keyboard=[
+                            [
+                                InlineKeyboardButton(
+                                    text="Обновить", callback_data="update:USD_RUB"
+                                )
+                            ]
+                        ]
+                    ),
+                )
+        elif symbol == "EUR_RUB":
+            price = await fetch_eur_rub()
+            if price:
+                await bot.edit_message_text(
+                    text=f"Текущий курс EUR/RUB: {price} RUB",
+                    inline_message_id=callback_query.inline_message_id,
+                    reply_markup=InlineKeyboardMarkup(
+                        inline_keyboard=[
+                            [
+                                InlineKeyboardButton(
+                                    text="Обновить", callback_data="update:EUR_RUB"
+                                )
+                            ]
+                        ]
+                    ),
+                )
     await bot.answer_callback_query(callback_query.id)
